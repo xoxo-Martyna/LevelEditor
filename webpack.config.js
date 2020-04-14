92289c6d2cf1149878059f64df267ceb361901c5
scintilla4evr <kacper17@outlook.com>
2020-04-14 15:47:44 +0200

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = [
+    {
+        mode: "development",
+        entry: "./src/index.ts",
+        target: "electron-main",
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    include: /src/,
+                    use: [
+                        {
+                            loader: "ts-loader"
+                        }
+                    ]
+                }
+            ]
+        },
+        output: {
+            path: __dirname + "/src",
+            filename: "index.js"
+        }
+    }
+]
